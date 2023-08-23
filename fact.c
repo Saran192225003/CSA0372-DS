0743af24f36a3c64bd56359e69f7e92a78d714a0
Saran192225003 <132343901+Saran192225003@users.noreply.github.com>
2023-08-23 19:28:48 +0530

    Add files via upload
    Enter a number: 5
    Factorial of 5 is 120
    
    --------------------------------
    Process exited after 2.564 seconds with return value 22
    Press any key to continue . . .

--- fact.c
+++ fact.c
@@ -1,12 +1,21 @@
 #include<stdio.h>  
-int main()    
-{    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  printf("Factorial of %d is: %d",number,fact);    
-return 0;  
-}   
+  
+long factorial(int n)  
+{  
+  if (n == 0)  
+    return 1;  
+  else  
+    return(n * factorial(n-1));  
+}  
+   
+void main()  
+{  
+  int number;  
+  long fact;  
+  printf("Enter a number: ");  
+  scanf("%d", &number);   
+   
+  fact = factorial(number);  
+  printf("Factorial of %d is %ld\n", number, fact);  
+  return 0;  
+}    
